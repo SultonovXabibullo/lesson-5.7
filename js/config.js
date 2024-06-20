ff847837c56923e6cc9c523cd0e3857e3f803118
SultonovXabibullo <sultonovxabibullo444@gmail.com>
2024-06-20 08:21:58 +0500

    first commit

---
+++ js/config.js
@@ -0,0 +1,6 @@
+let prompt1 = prompt("enter background color");
+let prompt2 = prompt("enter text color");
+let p = document.querySelector("p")
+let body = document.querySelector("body")
+p.style.color = prompt2;
+body.style.background = prompt1;
